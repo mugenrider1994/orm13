212c3015080e73e531b7777319c9d5fb845d26c1
Megan Glorioso <meganglorioso2016@gmail.com>
2023-07-03 18:09:49 -0700

    added routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -1,16 +1,32 @@
+const { Category, Product, Tag, ProductTag } = require('./models');
+
+// Use the models
+Category.findAll().then((categories) => {
+  console.log(categories);
+});
+
+
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
