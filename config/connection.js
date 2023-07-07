5b855836529a59ff3de7c2b38de27491a2fc7cee
Megan Glorioso <meganglorioso2016@gmail.com>
2023-07-06 17:45:48 -0700

    error fixing

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,6 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      user: 'root',
-      password: 'BreezySnipez20!',
-      database: 'ecommerce_db',
       dialectOptions: {
         decimalNumbers: true,
       },
